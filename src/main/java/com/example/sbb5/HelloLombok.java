d49a804703854864f0ed74592c329702cc3fc7be
JeaHukLim <a01036952858@gmail.com>
2024-09-14 14:49:21 +0900

    feat:@RequiredArgsConstructor추가

--- src/main/java/com/example/sbb5/HelloLombok.java
+++ src/main/java/com/example/sbb5/HelloLombok.java
@@ -1,18 +1,18 @@
 package com.example.sbb5;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+@RequiredArgsConstructor
 @Getter
-@Setter
+//@Setter
 public class HelloLombok {
-    private String hello;
-    private int lombok;
+    private final String hello;
+    private final int lombok;
 
     public static void main(String[] args){
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setHello("헬로");
-        helloLombok.setLombok(10);
+        HelloLombok helloLombok = new HelloLombok("헬로", 10);
 
         System.out.println(helloLombok.getHello());
         System.out.println(helloLombok.getLombok());
