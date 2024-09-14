ce446a1a4585b4081c556d37ef1f72b42ff9486e
JeaHukLim <a01036952858@gmail.com>
2024-09-14 14:35:45 +0900

    feat:Hello

---
+++ src/test/java/com/example/sbb5/Sbb5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
