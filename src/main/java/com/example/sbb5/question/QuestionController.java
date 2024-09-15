e372de9b0208b0ea7d3ebc5b054500e15525acb0
JeaHukLim <a01036952858@gmail.com>
2024-09-16 00:20:41 +0900

    feat:paging 추가

--- src/main/java/com/example/sbb5/question/QuestionController.java
+++ src/main/java/com/example/sbb5/question/QuestionController.java
@@ -3,12 +3,11 @@ package com.example.sbb5.question;
 import com.example.sbb5.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequestMapping("/question")
 @RequiredArgsConstructor
@@ -17,9 +16,9 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String questionList(Model model) {
-        List<Question> questionsList = this.questionService.getList();
-        model.addAttribute("questionList", questionsList);
+    public String questionList(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
